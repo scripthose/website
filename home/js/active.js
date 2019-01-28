6ad77db224ffb2124475a9aaf83f90f659c8e400
Elsiddig <elsiddig190@gmail.com>
2019-01-28 12:09:14 +0200

    edit the header area totally

--- home/js/active.js
+++ home/js/active.js
@@ -229,4 +229,20 @@
         })
     })
 
+    let navOffsetTop = $('header').height() + 50;
+
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= navOffsetTop ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
+
 })(jQuery);
