a374a05659d37a53666aa25da8a17b7d4506a6e1
Elsiddig <elsiddig190@gmail.com>
2019-01-30 13:31:21 +0200

    end up style for the blog

--- routing/route.js
+++ routing/route.js
@@ -1,74 +1,75 @@
-const models = require('../models/models');
+const models = require("../models/models");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.render("index", { title: "" });
+});
 
-router.get('/', (req, res) => {
-    res.render('index', {title: ''})
-})
+router.get("/home", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
 
-router.get('/home', (req, res) => {
-    res.render('index', {title: 'Home'})
-})
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact" });
+});
 
+router.get("/portfolio", (req, res) => {
+  res.render("portfolio", { title: "Protfolio" });
+});
 
-router.get('/about', (req, res) => {
-    res.render('about', {title: 'About'})
-})
+router.get("/cart", (req, res) => {
+  res.render("cart", { title: "Cart" });
+});
 
-router.get('/contact', (req, res) => {
-    res.render('contact', {title: 'Contact'})
-})
+router.get("/portfolio/project", (req, res) => {
+  res.render("single-portfolio", { title: "Protofolio" });
+});
 
+router.get("/blog", (req, res, next) => {
+  res.render("blog", { title: "Blog" });
+});
 
-router.get('/portfolio', (req, res) => {
-    res.render('portfolio', {title: 'Protfolio'})
-})
+router.get("/blog/questions/new/", (req, res) => {
+  res.render("question.blog.ejs", { title: "new question" });
+});
 
-router.get('/cart', (req, res) => {
-    res.render('cart', {title: 'Cart'})
-})
+router.get("/nubia", (req, res) => {
+  res.render("nubia-team", { title: "Nubia Team" });
+});
 
+router.get("/nubia/products", (req, res) => {
+  res.render("nubia", { title: "Nubia Product" });
+});
 
-router.get('/portfolio/project', (req, res) => {
-    res.render('single-portfolio', {title: 'Protofolio'})
-})
-
-router.get('/blog', (req, res, next) => {
-    res.render('blog', {title: 'Blog'})
-})
-
-router.get('/nubia', (req, res) => {
-    res.render('nubia-team', {title: 'Nubia Team'})
-})
-
-router.get('/nubia/products', (req, res) => {
-    res.render('nubia', {title: 'Nubia Product'})
-})
-
-router.get('/checkout', (req, res) => {
-    res.render('checkout', {title: 'Checkout'})
+router.get("/checkout", (req, res) => {
+  res.render("checkout", { title: "Checkout" });
 });
 
 // getting id param and load the data on the blog post
-router.get('/nubia/products/product_number=:id', (req, res) => {
-    global.shopIdUrl = req.params.id;
+router.get("/nubia/products/product_number=:id", (req, res) => {
+  global.shopIdUrl = req.params.id;
 
-    models.nubiaProduct.findOne({_id: shopIdUrl}, function(err, data){
-        if(err) return console.error(err);
-        res.render('shop', {
-            title: 'Shop',
-            prodName: data.prodName,
-            prodPic: data.prodPic,
-            prodPrice: data.prodPrice,
-            prodDesc: data.prodDesc
-        });
-
-
+  models.nubiaProduct.findOne({ _id: shopIdUrl }, function(err, data) {
+    if (err) return console.error(err);
+    res.render("shop", {
+      title: "Shop",
+      prodName: data.prodName,
+      prodPic: data.prodPic,
+      prodPrice: data.prodPrice,
+      prodDesc: data.prodDesc
     });
+  });
+});
 
-})
+// new blog question
 
+router.post("/blog/questions/question-id=", (req, res) => {
+  res.render("question", { title: "Forum" });
+});
 
 module.exports.getPages = router;
