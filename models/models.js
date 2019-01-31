2809cba3bd53b2ef640d2a6fddb107eb808623f7
Yassen Higazi <yassenka28@gmail.com>
2019-01-31 07:08:30 +0200

    add question, tag and answer models

--- models/models.js
+++ models/models.js
@@ -2,6 +2,27 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const Question = new Schema({
+	title: String,
+	body: String,
+	tags: [{type: Schema.Types.ObjectId, ref:'tags'}],
+	answers: [{type: Schema.Types.ObjectId, ref: 'answers'}],
+	views: Number
+});
+
+const Tag = new Schema({
+	title: String,
+	questions: [{type: Schema.Types.ObjectId, ref: 'questions'}]
+});
+
+const Answer = new Schema({
+	question: {type: Schema.Types.ObjectId, ref: 'questions'},
+	body: String,
+	votes: {total: Number, up: Number, down: Number},
+	isCorrect: Boolean,
+	views: Number
+});
+
 // this model is made for script house users
 const scriptorSchema = new Schema({
 	scriptorName: String,
@@ -28,8 +49,11 @@ const nubiaProductSchema = new Schema({
 })
 
 // create the schema on the database
-const scriptor = mongoose.model('scriptor', scriptorSchema);
-const nubiaProduct = mongoose.model('nubiaProduct', nubiaProductSchema);
+const tag = mongoose.model('tags', Tag);
+const answer = mongoose.model('answers', Answer);
+const question = mongoose.model('questions', Question);
+const scriptor = mongoose.model('scriptors', scriptorSchema);
+const nubiaProduct = mongoose.model('nubiaProducts', nubiaProductSchema);
 
 
 // let prod = {
@@ -40,26 +64,39 @@ const nubiaProduct = mongoose.model('nubiaProduct', nubiaProductSchema);
 // 	prodLove: 1
 // }
 
-// let postTest = {
-// 	postTitle: 'Simple',
-// 	postPic: 'http://localhost:2000/img/bg-img/46.png',
-// 	postDate: new Date(Date.now()),
-// 	postEditor: 'Yassen',
-// 	postTopic: 'Test',
-// 	postSubTitle: "simplicty is the greatest sophistication",
-// 	subTitconstopic: "Unknown",
-// 	comments: [] 
+// let q = {
+// 	title: 'Question',
+// 	body: 'test data',
+// 	tags: [],
+// 	answers: [],
+// 	views: 1
+// }
+
+// let ans = {
+// 	question: null,
+// 	body: 'Answer',
+// 	votes: {total: 12, up: 9, down: 3},
+// 	isCorrect: true,
+// 	views: 20
+// }
+
+// let t = {
+// 	title: "test",
+// 	questions: []
 // }
 
 // new nubiaProduct(prod).save().then((data) => console.log(data));
-// new post(postTest).save().then((data) => console.log(data));
-
-
+// new question(q).save().then((data) => console.log(data));
+// new answer(ans).save().then((data) => console.log(data));
+// new tag(t).save().then((data) => console.log(data));
 
 // exporting the models outside of the 
 module.exports = {
 	scriptor,
-	nubiaProduct
+	nubiaProduct,
+	question,
+	answer,
+	tag
 }
 
 
